6fbafae5aabce44d7a1cc8135bdc7b460a1ba682
thy <972143891@qq.com>
2019-07-14 17:28:13 +0800

    feat: add font

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,23 +1,46 @@
 <template>
   <div class="df footer">
     <div class="footer-nav flex-column flex1 tc">
-      <router-link to="/">message</router-link>
+      <router-link to="/" @click.native="onClickNav('1')">
+        <span class="iconfont">&#xe652;</span>
+        <div>X 信</div>
+      </router-link>
     </div>
     <div class="footer-nav flex-column flex1 tc">
-      <router-link to="/address">address</router-link>
+      <router-link to="/address">
+        <span class="iconfont">&#xe624;</span>
+        <div>通讯录</div>
+      </router-link>
     </div>
     <div class="footer-nav flex-column flex1 tc">
-      <router-link to="/find">find</router-link>
+      <router-link to="/find">
+        <span class="iconfont">&#xe628;</span>
+        <div>发现</div>
+      </router-link>
     </div>
     <div class="footer-nav flex-column flex1 tc">
-      <router-link to="/mine">mine</router-link>
+      <router-link to="/mine">
+        <span class="iconfont">&#xe61d;</span>
+        <div>我</div>
+      </router-link>
     </div>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 export default {
   name: 'Footer',
+  data() {
+    return {
+      active:'1'
+    }
+  },
+  methods: {
+    onClickNav(mark) {
+      console.info(mark);
+    }
+  }
 };
 </script>
 
@@ -31,5 +54,15 @@ export default {
 
 .footer-nav {
   justify-content: center;
+}
+
+.footer-nav .router-link-exact-active {
+  color: rgba(68,199,98,1);
+}
+
+.footer-nav .iconfont {
+  display: inline-block;
+  margin-bottom: 3px;
+  font-size: 20px;
 }
 </style>
